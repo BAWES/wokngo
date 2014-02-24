b3cb8ca3d4ac38e0f439d85021973b2d98bc9579
khalidmnet <khalid@khalidm.net>
2014-02-24 20:14:37 +0300

    Updated SQL Export + Added comments to About page

--- protected/views/site/about.php
+++ protected/views/site/about.php
@@ -7,3 +7,17 @@ $this->pageHeader = "About";
 ?>
 
 <p>About Wok&amp;Go content will go here</p>
+
+<?php
+//HERE WE WILL START WORKING ON RANKING SYSTEM
+//ADD getRank to each Items model
+
+//Items ordered by Sales
+$items = Item::model()->with(array(
+                    'totalSold' => array(
+                        'select' => true,
+                        'joinType' => 'INNER JOIN',
+                        'order' => 'totalSold DESC'
+            )))->findAll();
+
+?>
