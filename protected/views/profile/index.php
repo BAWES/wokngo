f007738a9bb07bd7a23d066e669bc406150f3092
khalidmnet <khalid@khalidm.net>
2014-03-07 00:13:00 +0300

    Updated box profile page

--- protected/views/profile/index.php
+++ protected/views/profile/index.php
@@ -9,7 +9,7 @@ $this->pageHeader = "Profile";
 <h1>Welcome <?php echo $user->customer_name; ?></h1>
 <ul>
     <li><a href='#changePw'>Change Password</a></li>
-    <li><a href='#changePw'>Logout</a></li>
+    <li><a href='<?php echo Yii::app()->createUrl('site/logout');?>'>Logout</a></li>
 </ul>
 <h2>My Boxes</h2><br/>
 
@@ -17,10 +17,15 @@ $this->pageHeader = "Profile";
 <?php foreach($user->items as $box){ ?>
     <tr>
         <td>
-            <img src='<?php echo $box->image; ?>'/>
+            <img src='<?php echo $box->image; ?>' style='width:50px; height:50px;'/>
         </td>
         <td>
-            <?php echo $box->item_name; ?>
+            <a href="<?php echo Yii::app()->createUrl('box/view',array('seo'=>$box->item_seo_name)); ?>">
+                <?php echo $box->item_name; ?>
+            </a>
+        </td>
+        <td>
+            <?php echo $box->item_description; ?>
         </td>
     </tr>
 <?php } ?>
