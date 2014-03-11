b98e230b790ce8492ccc92bdd97b483171f36d32
khalidmnet <khalid@khalidm.net>
2014-03-11 16:44:12 +0300

    Updated profile view

--- protected/views/profile/index.php
+++ protected/views/profile/index.php
@@ -2,31 +2,32 @@
 /* @var $this ProfileController */
 /* @var $user Customer */
 
-$this->pageTitle=Yii::app()->name . ' - '.$user->customer_name;
+$this->pageTitle = Yii::app()->name . ' - ' . $user->customer_name;
 $this->pageHeader = "Profile";
 ?>
 
 <h1>Welcome <?php echo $user->customer_name; ?></h1>
 <ul>
     <li><a href='#changePw'>Change Password</a></li>
-    <li><a href='<?php echo Yii::app()->createUrl('site/logout');?>'>Logout</a></li>
+    <li><a href='<?php echo Yii::app()->createUrl('site/logout'); ?>'>Logout</a></li>
 </ul>
 <h2>My Boxes</h2><br/>
 
-<table>
-<?php foreach($user->items as $box){ ?>
-    <tr>
-        <td>
-            <img src='<?php echo $box->image; ?>' style='width:50px; height:50px;'/>
-        </td>
-        <td>
-            <a href="<?php echo Yii::app()->createUrl('box/view',array('seo'=>$box->item_seo_name)); ?>">
-                <?php echo $box->item_name; ?>
-            </a>
-        </td>
-        <td>
-            <?php echo $box->item_description; ?>
-        </td>
-    </tr>
-<?php } ?>
+<table style="width:100%;">
+    <?php foreach ($user->items as $box) { ?>
+        <tr>
+            <td style="width:60px;">
+                <a href="<?php echo Yii::app()->createUrl('box/view', array('seo' => $box->item_seo_name)); ?>">
+                    <img src='<?php echo $box->image; ?>' style='width:50px; height:50px;'/>
+                </a>
+            </td>
+            <td>
+                <a href="<?php echo Yii::app()->createUrl('box/view', array('seo' => $box->item_seo_name)); ?>">
+                    <?php echo $box->item_name; ?>
+                </a>
+            </td>
+            <td><a href="#">Change Logo</a></td>
+            <td><a href="#">Change Description</a></td>
+        </tr>
+    <?php } ?>
 </table>
