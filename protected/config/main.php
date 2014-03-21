b2fd158606c2fece399b0d015b1dd3d69900686a
khalidmnet <khalid@khalidm.net>
2014-03-21 15:23:03 +0300

    Disabled Gii

--- protected/config/main.php
+++ protected/config/main.php
@@ -18,12 +18,14 @@ return array(
     'modules' => array(
         // uncomment the following to enable the Gii tool
         'admin',
+        /*
         'gii' => array(
             'class' => 'system.gii.GiiModule',
             'password' => '12345',
             // If removed, Gii defaults to localhost only. Edit carefully to taste.
             'ipFilters' => array('127.0.0.1', '::1'),
         ),
+         */
     ),
     // application components
     'components' => array(
