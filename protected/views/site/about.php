760d9c82ade5c0fa39b682d11c8803c91e80a513
khalidmnet <khalid@khalidm.net>
2014-03-30 11:19:08 +0300

    Updated franchise and about pages

--- protected/views/site/about.php
+++ protected/views/site/about.php
@@ -6,40 +6,25 @@ $this->pageTitle = Yii::app()->name . ' - About Us';
 $this->pageHeader = "About Us";
 ?>
 
-<!-- Slider -->
-<div class="slider">
-    <ul>
-        <li>
-            <h1>Sweet Chili</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <a href="#">Our Menu</a>
-        </li>
-        <li style="background-color:#fa9312;">
-            <h1>Order Online!</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <a href="#">Order Now</a>
-        </li>
-        <li style="background-color:#10b58c;">
-            <h1>Franchise</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <a href="#">More Info</a>
-        </li>
-    </ul>
+<div class="sami1">
+
+    <header id='page'>
+        <h1 style="margin-top:0.7em; color:white;">About Us</h1>
+    </header>
+
+    <section id="pageContent">
+        <div class="sami2">
+            <p>
+                What inspires a young English man to launch an innovative, authentic Asian fast food concept in the UK? 
+            </p>
+            <p>
+                A love of the taste and aromas of the East and time spent in the Asian dining districts of the Big Apple.<br/> “I got home from a trip to New York in 2007 reeling with a sense of opportunity and excitement about the myriad of quick, casual, convenient dining experiences on offer. I wanted to recreate authentic, Asian cuisine in a casual and relaxed atmosphere. For me, the answer was Wok&Go,<br/> a tasty, healthy, fresh and nutritious Asian food cooked to order in minutes before the customers’ eyes, that they could eat on the hop at simple tables and benches or take away in a box.” 
+            </p>
+            <p>
+                Wok&Go first opened its doors in Chester in 2008 and has grown into a unique brand led fusion food chain blending the best of Thai, Malaysian, Indonesian, Chinese and Japanese cuisine. Now with franchises across the North West, North East, Southern England and the Middle East, with Mainland Europe and the USA coming soon...<br/><br/>Wok&Go has become the UK's number one Noodle Bar Chain.
+            </p>
+        </div>
+        <div style="clear:both;"></div>
+    </section>
+
 </div>
-
-<header id='page' style="background:none; height:auto;">
-    <h1 style="color:black; margin-top:0.7em;">About Us</h1>
-</header>
-
-<section id="pageContent" style="">
-    <p style="text-align:justify;">
-        What inspires a young English man to launch an innovative, authentic Asian fast food concept in the UK? 
-    </p>
-    <p style="text-align:justify;">
-        A love of the taste and aromas of the East and time spent in the Asian dining districts of the Big Apple. “I got home from a trip to New York in 2007 reeling with a sense of opportunity and excitement about the myriad of quick, casual, convenient dining experiences on offer. I wanted to recreate authentic, Asian cuisine in a casual and relaxed atmosphere. For me, the answer was Wok&Go, a tasty, healthy, fresh and nutritious Asian food cooked to order in minutes before the customers’ eyes, that they could eat on the hop at simple tables and benches or take away in a box.” 
-    </p>
-    <p style="text-align:justify;">
-        Wok&Go first opened its doors in Chester in 2008 and has grown into a unique brand led fusion food chain blending the best of Thai, Malaysian, Indonesian, Chinese and Japanese cuisine. Now with franchises across the North West, North East, Southern England and the Middle East, with Mainland Europe and the USA coming soon...Wok&Go has become the UK's number one Noodle Bar Chain.
-    </p>
-</section>
-
--- protected/views/site/franchise.php
+++ protected/views/site/franchise.php
@@ -6,64 +6,48 @@ $this->pageTitle = Yii::app()->name . ' - Franchise';
 $this->pageHeader = "Franchise";
 ?>
 
-<!-- Slider -->
-<div class="slider">
-    <ul>
-        <li>
-            <h1>Sweet Chili</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <a href="#">Our Menu</a>
-        </li>
-        <li style="background-color:#fa9312;">
-            <h1>Order Online!</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <a href="#">Order Now</a>
-        </li>
-        <li style="background-color:#10b58c;">
-            <h1>Franchise</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <a href="#">More Info</a>
-        </li>
-    </ul>
+<div class="sami1" style="background-color: #10b58c;">
+    <header id='page'>
+        <h1 style="margin-top:0.7em; color:white;">Franchise</h1>
+    </header>
+
+    <section id="pageContent">
+        <div class="sami3">
+            <p>
+                Wok&Go first opened its doors in Chester in 2008 and has grown into a unique brand led fusion food chain blending the best of Thai, Malaysian, Indonesian, Chinese and Japanese cuisine. Now with franchises across the North West, North East, Southern England and the Middle East, with Mainland Europe and the USA coming soon...Wok&Go has become the UK's number one Noodle Bar Chain. 
+            </p>
+            <p>
+                What makes the ideal Wok&Go franchise? 
+            </p>
+            <p>
+                The secret ingredient…<span style="font-weight:bold;">you! </span>
+            </p>
+            <p>
+                Our franchisees tend to have some traits in common: 
+            </p>
+            <p>
+                <span style="font-weight:bold;">Self-motivated:</span> <br/>
+                You need to be driven, ambitious, disciplined and determined to succeed
+            </p>
+            <p>
+                <span style="font-weight:bold;">Organized:</span> <br/>
+                You need to be good at juggling (not knives) but many different and interdependent tasks
+            </p>
+            <p>
+                <span style="font-weight:bold;">Motivational:</span> <br/>
+                Getting the best from your team is critical to your franchise’s reputation and financial success
+            </p>
+            <p>
+                <span style="font-weight:bold;">Customer service focused:</span> <br/>
+                Dealing with the public takes enthusiasm, tenacity and a happy smile!
+            </p>
+            <p>
+                You don’t need previous retail or catering experience, but some management experience is an advantage in running your own demanding and exciting retail business. 
+            </p>
+            <p>
+                Thank you for considering a Wok&Go franchise as your future business venture.<br/><br/> For more information, please contact us at <a href="mailto:franchise@wokandgo.me" style="color:white; font-weight:bold; display:inline;">franchise@wokandgo.me</a>
+            </p>
+        </div>
+        <div style="clear:both;"></div>
+    </section>
 </div>
-
-<header id='page' style="background:none; height:auto;">
-    <h1 style="color:black; margin-top:0.7em;">Franchise</h1>
-</header>
-
-<section id="pageContent">
-    <p  style="text-align:justify;">
-        Wok&Go first opened its doors in Chester in 2008 and has grown into a unique brand led fusion food chain blending the best of Thai, Malaysian, Indonesian, Chinese and Japanese cuisine. Now with franchises across the North West, North East, Southern England and the Middle East, with Mainland Europe and the USA coming soon...Wok&Go has become the UK's number one Noodle Bar Chain. 
-    </p>
-    <p  style="text-align:justify;">
-        What makes the ideal Wok&Go franchise? 
-    </p>
-    <p  style="text-align:justify;">
-        The secret ingredient…<span style="color:#16a0da;">you! </span>
-    </p>
-    <p  style="text-align:justify;">
-        Our franchisees tend to have some traits in common: 
-    </p>
-    <p  style="text-align:justify;">
-        <span style="color:#16a0da;">Self-motivated:</span> 
-        You need to be driven, ambitious, disciplined and determined to succeed
-    </p>
-    <p  style="text-align:justify;">
-        <span style="color:#16a0da;">Organized:</span> 
-        You need to be good at juggling (not knives) but many different and interdependent tasks
-    </p>
-    <p  style="text-align:justify;">
-        <span style="color:#16a0da;">Motivational:</span> 
-        Getting the best from your team is critical to your franchise’s reputation and financial success
-    </p>
-    <p  style="text-align:justify;">
-        <span style="color:#16a0da;">Customer service focused:</span> 
-        Dealing with the public takes enthusiasm, tenacity and a happy smile!
-    </p>
-    <p  style="text-align:justify;">
-        You don’t need previous retail or catering experience, but some management experience is an advantage in running your own demanding and exciting retail business. 
-    </p>
-    <p  style="text-align:justify;">
-        Thank you for considering a Wok&Go franchise as your future business venture. For more information, please contact us.
-    </p>
-</section>
