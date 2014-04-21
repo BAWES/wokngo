592691cc5143e76f7c4f029b5f3c8d387eeba2a2
khalidmnet <khalid@khalidm.net>
2014-04-21 11:41:01 +0300

    Added locations tab

---
+++ protected/views/site/locations.php
@@ -0,0 +1,14 @@
+<?php
+/* @var $this SiteController */
+/* @var $error array */
+
+$this->pageTitle = Yii::app()->name . ' - Locations';
+$this->pageHeader = "Locations";
+?>
+
+<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?t=m&amp;sll=29.3790105,47.9928838&amp;sspn=0.0071801,0.0109864&amp;q=Al+Hamra+Tower+%26+Mall,+Al-Shuhada+St&amp;ie=UTF8&amp;hq=&amp;hnear=Al+Hamra+Tower+%26+Mall,+Al-Shuhada+St,+Sharq,+Al+Kuwayt,+Al+Asimah,+Kuwait&amp;z=14&amp;iwloc=A&amp;ll=29.379024,47.992824&amp;output=embed"></iframe>
+<br/><br/>
+<p>
+    <b>Telephone:</b> +965 2220 2225
+    <p><b>Address:</b> AlHamra Luxury Center, Sharq Area, Al-Shuhada St. Kuwait City, First Floor</p>
+</p>
